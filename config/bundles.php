4623c024b7e422288330b297c7fe05659fef613d
Hamdi AFRIT <hamdi.afrit@external.grdf.fr>
2020-10-27 15:00:07 +0100

    add profiler

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
